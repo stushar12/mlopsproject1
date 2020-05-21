8d11d8f5fe34feb8cbe28eb441c137cb8d23379b
Tushar Saxena <stushar345@gmail.com>
2020-05-22 00:17:35 +0530

    New

---
+++ lessaccuracy_notifier.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+host_address = "stushar@gmail.com"
+host_pass = "CSE1805718"
+guest_address = "1805718@gmail.com"
+subject = "Regarding failure of main.py having less accuracy then the desired one"
+content = '''Hello, 
+				Developer this is an email regarding to your last commit. It seems that your main.py has not reached to the desired accuracy level.
+			THANK YOU'''
+message = MIMEMultipart()
+message['From'] = host_address
+message['To'] = guest_address
+message['Subject'] = subject
+message.attach(MIMEText(content, 'plain'))
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.starttls()
+session.login(host_address, host_pass)
+text = message.as_string()
+session.sendmail(host_address, guest_address  , text)
+session.quit()
+print('Successfully sent your mail')
+
